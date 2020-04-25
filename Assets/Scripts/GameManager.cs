d4dbba37ec8e4cb11c3cfcb64869be2dc36b7e77
TaboTavin <christian.martinez@saei.mx>
2020-04-25 07:25:35 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GameState
+{
+    inGame,
+    pause,
+    gameOver
+}
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager sharedInstance;
+
+    public GameState currentGameState = GameState.pause;
+
+    private void Awake()
+    {
+        if (sharedInstance == null)
+            sharedInstance = this;
+
+        else
+            Destroy(gameObject);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+            StartGame();
+
+        if (Input.GetKeyDown(KeyCode.P))
+            PauseGame();
+        
+    }
+    public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+    }
+
+    public void PauseGame()
+    {
+        SetGameState(GameState.pause);
+    }
+
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    private void SetGameState(GameState newGameState)
+    {
+        if(newGameState == GameState.inGame)
+        {
+            //Logica para mostrar UI
+        }
+
+        else if(newGameState == GameState.pause)
+        {
+            //Logica para mostrar la pausa
+        }
+
+        else if(newGameState == GameState.gameOver)
+        {
+            //Logica para mostrar GameOver
+        }
+
+        this.currentGameState = newGameState;
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    Vector3 movement;
+
+    public Rigidbody playerRB;
+
+   
+    void Update()
+    {
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.z = Input.GetAxisRaw("Vertical");
+
+            playerRB.MovePosition(playerRB.position + movement * Time.deltaTime);
+        }
+        
+    }
+}
